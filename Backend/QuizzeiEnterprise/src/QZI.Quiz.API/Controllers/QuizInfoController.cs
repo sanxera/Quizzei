8a6835f4e4fe70dfce736e9ea94490e10f8a1167
sanxera <guilherme.sanchesVS@hotmail.com>
2022-04-12 19:55:29 -0300

    feat: add changes

---
+++ Backend/QuizzeiEnterprise/src/QZI.Quiz.API/Controllers/QuizInfoController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QZI.Core.Controllers;
+
+namespace QZI.Quiz.API.Controllers
+{
+    public class QuizInfoController : MainController
+    {
+        public async Task<IActionResult> CreateQuizInfo()
+        {
+            return Ok();
+        }
+    }
+}
