4a8bb7e64bb4abf576001fc6a4403e14efd24073
Guilherme Henrique Antunes Sanches <guilhermes.sanches@neon.com.br>
2022-05-04 17:17:30 -0300

    feat: add rpo

--- Backend/QuizzeiEnterprise/src/QZI.Quiz.Domain/Quiz/Handlers/QuestionCommandHandler.cs
+++ Backend/QuizzeiEnterprise/src/QZI.Quiz.Domain/Quiz/Handlers/QuestionCommandHandler.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using QZI.Quiz.Domain.Quiz.Handlers.Commands.Question;
+using QZI.Quiz.Domain.Quiz.Handlers.Requests.Questions;
 using QZI.Quiz.Domain.Quiz.Handlers.Response.Question;
 using QZI.Quiz.Domain.Quiz.Repositories;
 
@@ -10,11 +11,13 @@ namespace QZI.Quiz.Domain.Quiz.Handlers
 {
     public class QuestionCommandHandler : IRequestHandler<CreateQuestionsCommand, CreateQuestionsResponse>
     {
+        private readonly IQuizInfoRepository _quizInfoRepository;
         private readonly IQuestionRepository _questionRepository;
 
-        public QuestionCommandHandler(IQuestionRepository questionRepository)
+        public QuestionCommandHandler(IQuestionRepository questionRepository, IQuizInfoRepository quizInfoRepository)
         {
             _questionRepository = questionRepository;
+            _quizInfoRepository = quizInfoRepository;
         }
 
         public Task<CreateQuestionsResponse> Handle(CreateQuestionsCommand request, CancellationToken cancellationToken)
@@ -22,6 +25,16 @@ namespace QZI.Quiz.Domain.Quiz.Handlers
             request.Validate();
             
             throw new NotImplementedException();
+        }
+
+        private void ValidateQuizInfo()
+        {
+
+        }
+
+        private void CreateNewQuestions(CreateQuestionsRequest request)
+        {
+
         }
     }
 }
