772e029d115f7187eff7160665676070001771d7
Luizt07 <luiz_1609@hotmail.com>
2022-11-08 18:00:49 -0300

    fix: siderMenu & logo

--- front-end/src/components/Layout/SiderMenu.js
+++ front-end/src/components/Layout/SiderMenu.js
@@ -1,19 +1,14 @@
 import React, { useState } from 'react';
-import { Layout, Menu, Avatar, Typography, Divider } from 'antd';
 import { useNavigate } from 'react-router-dom';
-import {
-  LogoutOutlined,
-  SettingOutlined,
-  MailOutlined,
-  AppstoreOutlined
-} from '@ant-design/icons';
+import { Layout, Menu, Avatar, Typography, Divider } from 'antd';
+import { Gauge, ChatCenteredText, Folder, Gear, SignOut, UserFocus } from 'phosphor-react'
 import logoQuizzei from '../../image/logo-quizzei.png';
 import { setAuthority } from '../../utils/auth';
 
 import styles from './styles.less'
 
 const { Sider } = Layout;
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 const SiderMenu = () => {
   const navigate = useNavigate();
@@ -33,31 +28,31 @@ const SiderMenu = () => {
     {
       label: 'Dashboard',
       key: 'dashboard',
-      icon: <MailOutlined />,
+      icon: <Gauge size={20} />,
       onClick: () => navigate('/dashboard')
     },
     {
       label: 'Quizzes',
       key: 'quiz',
-      icon: <MailOutlined />,
+      icon: <ChatCenteredText size={20} />,
       onClick: () => navigate('/quiz')
     },
     {
       label: 'Conteúdo',
       key: 'content',
-      icon: <AppstoreOutlined />,
+      icon: <Folder size={20} />,
       onClick: () => navigate('/content')
     },
     {
       label: 'Configurações',
       key: 'configuration',
-      icon: <SettingOutlined />,
+      icon: <Gear size={20} />,
       onClick: () => navigate('/user')
     },
     {
       label: 'Sair',
       key: 'logout',
-      icon: <LogoutOutlined />,
+      icon: <SignOut size={20} />,
       onClick: () => logout()
     },
   ];
@@ -73,25 +68,27 @@ const SiderMenu = () => {
         left: 0,
         top: 0,
         bottom: 0,
+        padding: 10,
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         textAlign: 'center',
-        backgroundColor: '#47a7f0'
+        backgroundColor: '#47a7f0',
       }}
       trigger={null}>
-      <div className="logo">
+      <div className={styles.logo}>
         <img
           style={{ height: 63 }}
           src={logoQuizzei}
         />
       </div>
-
+      <Divider style={{ backgroundColor: '#fff' }} />
       <div style={{ marginTop: 20 }}>
         <Avatar
           style={{
             backgroundColor: '#f56a00',
             verticalAlign: 'middle',
+            border: '1px solid #fff'
           }}
           size={64}
           gap={4}
@@ -99,9 +96,14 @@ const SiderMenu = () => {
 
         <Title style={{ color: 'white', marginTop: 20 }} level={3}>Luiz Eduardo</Title>
 
+        {/* <div>
+          <div style={{ display: 'flex', alignItems: 'center', color: '#fff', grid: 20 }}>
+            <UserFocus size={20} /> <Text style={{ color: '#fff' }}>Aluno</Text>
+          </div>
+        </div> */}
       </div>
       <Divider style={{ backgroundColor: '#fff' }} />
-      <Menu style={{ backgroundColor: '#47a7f0', color: '#fff', marginTop: 25, marginLeft: 25 }} mode="inline" defaultSelectedKeys={['4']} items={items} />
+      <Menu style={{ backgroundColor: '#47a7f0', color: '#fff', marginTop: 25, marginLeft: 13 }} mode="inline" defaultSelectedKeys={['4']} items={items} />
 
     </Sider>
   )
