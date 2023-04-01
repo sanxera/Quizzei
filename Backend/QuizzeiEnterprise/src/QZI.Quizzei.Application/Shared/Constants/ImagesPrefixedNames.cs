267e13565029264d9cd65d3215d05c7720e7dc24
Guilherme Sanches <guilherme.sanchesVS@hotmail.com>
2023-04-01 15:57:34 -0300

    feat: new image

--- Backend/QuizzeiEnterprise/src/QZI.Quizzei.Application/Shared/Constants/ImagesPrefixedNames.cs
+++ Backend/QuizzeiEnterprise/src/QZI.Quizzei.Application/Shared/Constants/ImagesPrefixedNames.cs
@@ -7,6 +7,7 @@ public static class ImagesPrefixedNames
     public const string Image3 = "image-mathematics.png";
     public const string Image4 = "image-pirate-story.png";
     public const string Image5 = "image-science.png";
+    public const string Image6 = "Default.png";
 
     public static string[] GetAllImages() =>
         new[]
@@ -15,6 +16,7 @@ public static class ImagesPrefixedNames
             Image2,
             Image3,
             Image4,
-            Image5
+            Image5,
+            Image6
         };
 }
