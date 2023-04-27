e2a810593411c01cefc26fb1e229dc834558e36a
Luizt07 <luiz_1609@hotmail.com>
2023-04-27 18:13:17 -0300

    fix: quiz slider

--- front-end/src/components/Card/SliderCard.js
+++ front-end/src/components/Card/SliderCard.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button as ButtonAntd } from 'antd';
+import { useKeenSlider } from 'keen-slider/react';
 import Card from './index';
 import { DEFAULT_THEME } from '../../utils/constant';
 
 import './index.less';
 
-const SliderCard = ({ data, openInfoQuizzes }) => {
-  if (!data.quizzesInfoResponses || data.quizzesInfoResponses.length === 0) {
+const SliderCard = ({ data, openInfoQuizzes, isMyQuiz }) => {
+  const [sliderRef] = useKeenSlider({
+    breakpoints: {
+      "(min-width: 1000px)": {
+        slides: { perView: 4.1, spacing: 2 },
+      },
+    },
+    dragSpeed: 3
+  })
+
+  if (!data || data.length === 0) {
     return (<ButtonAntd style={{ width: '80vw', minHeight: 100 }} type='dashed'>Não há quizzes</ButtonAntd>)
   }
 
   return (
-    <div className="slider-card">
-      {data.quizzesInfoResponses.map((item, index) => (
-        <Card
-          className="card"
-          key={`quizzes-${index}`}
-          isQuiz
-          logo={item.imageUrl || DEFAULT_THEME}
-          title={item.title}
-          description={item.description}
-          ownerNickName={item.ownerNickName}
-          numberOfQuestions={item.numberOfQuestions}
-          permissionType={item.permissionType || 1}
-          onClick={() => openInfoQuizzes(item)}
-          style={{ marginRight: 30, marginLeft: 0, minHeight: '20rem', padding: 0, width: 240 }}
-        />
+    <div ref={sliderRef} className="keen-slider">
+      {data.map((item, index) => (
+        <div className="keen-slider__slide card">
+          <Card
+            className="card1"
+            key={`quizzes-${index}`}
+            isQuiz
+            logo={item.imageUrl || DEFAULT_THEME}
+            title={item.title}
+            description={item.description}
+            ownerNickName={item.ownerNickName}
+            numberOfQuestions={item.numberOfQuestions}
+            permissionType={item.permissionType || 1}
+            onClick={() => openInfoQuizzes(item)}
+            isMyQuiz={isMyQuiz}
+            style={{ marginRight: 30, marginLeft: 0, minHeight: '20rem', padding: 0, width: 240 }}
+          />
+        </div>
       ))}
     </div>
   )
