b8625aeda3d07828f9b6e66c46de50a6f5bff517
Luizt07 <luiz_1609@hotmail.com>
2023-07-03 20:25:12 -0300

    fix

--- front-end/src/pages/Report/Items/ReportQuestionsCategory.js
+++ front-end/src/pages/Report/Items/ReportQuestionsCategory.js
@@ -14,11 +14,6 @@ const ReportQuestionsCategory = ({ data }) => {
         {data.questionsCategories && (
           <Collapse defaultActiveKey={['1']}>
             {data?.questionsCategories.map(category => {
-              const totalSeconds = category?.averageTimer || 0;
-
-              const hours = Math.floor(totalSeconds / 3600);
-              const minutes = Math.floor((totalSeconds / 3600) / 60);
-              const seconds = totalSeconds % 60;
               return (
                 <Panel header={`${category?.questionCategoryDescription}`}>
                   <Row>
@@ -41,62 +36,72 @@ const ReportQuestionsCategory = ({ data }) => {
                             <Divider style={{ height: '100%' }} type="vertical" />
                           </Col>
 
-                          <Col span={5}>
-                            <strong>{hours.toString().padStart(2, '0')}:{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</strong> <br /> <strong>Tempo médio</strong>
-                          </Col>
+
                         </Row>
                       </Card>
                     </Col>
 
                     <Col span={24}>
                       <Collapse defaultActiveKey={['1']} ghost>
-                        {category?.questions.length > 0 && category.questions.map(question => (
-                          <Panel
-                            header={
-                              <Text ellipsis={{ rows: 1 }}>{question.description}</Text>
-                            }
-                          >
-                            <Row type="flex" justify='start'>
-                              <Col span={5}>
-                                <Card style={{ width: '100%', textAlign: 'center' }}>
-                                  <strong>{question.totalAnswers}</strong> <br /> <strong>Total de respostas</strong>
-                                </Card>
-                              </Col>
+                        {category?.questions.length > 0 && category.questions.map(question => {
+                          const totalSeconds = question?.averageTimer || 0;
 
-                              <Col span={5}>
-                                <Card style={{ width: '100%', textAlign: 'center' }}>
-                                  <strong>{question.totalHitPercentage}%</strong> <br /> <strong>Acertos</strong>
-                                </Card>
-                              </Col>
-                            </Row>
+                          const hours = Math.floor(totalSeconds / 3600);
+                          const minutes = Math.floor((totalSeconds / 3600) / 60);
+                          const seconds = totalSeconds % 60;
+                          return (
+                            <Panel
+                              header={
+                                <Text ellipsis={{ rows: 1 }}>{question.description}</Text>
+                              }
+                            >
+                              <Row type="flex" justify='start'>
+                                <Col span={5}>
+                                  <Card style={{ width: '100%', textAlign: 'center' }}>
+                                    <strong>{question.totalAnswers}</strong> <br /> <strong>Total de respostas</strong>
+                                  </Card>
+                                </Col>
 
-                            <Row type="flex" justify='start' style={{ marginTop: 10 }}>
-                              <Col span={24}>
-                                <Text strong>Opções</Text>
-                              </Col>
-                              <Col span={24}>
-                                <div style={{ display: 'flex', flexDirection: 'column', marginLeft: 20 }}>
-                                  {question.options.map((option, index) => {
-                                    const code = 'a'.charCodeAt(0);
-                                    const letterOption = String.fromCharCode(code + index);
-                                    const typeCheck = option.isCorrect === true ? 'success' : 'danger';
-                                    return (
-                                      <div className={option.userCheck === true ? `question-option-check-${typeCheck}` : null}>
-                                        <Text
-                                          type={typeCheck}
-                                        >
-                                          {letterOption.toUpperCase()}. {option.description} ({option.totalOptionAnswersPercentage}%)
-                                        </Text>
-                                      </div>
-                                    )
-                                  })}
-                                </div>
-                              </Col>
-                            </Row>
+                                <Col span={5}>
+                                  <Card style={{ width: '100%', textAlign: 'center' }}>
+                                    <strong>{question.totalHitPercentage}%</strong> <br /> <strong>Acertos</strong>
+                                  </Card>
+                                </Col>
+                                <Col span={5}>
+                                  <Card style={{ width: '100%', textAlign: 'center' }}>
+                                    <strong>{hours.toString().padStart(2, '0')}:{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</strong> <br /> <strong>Tempo médio</strong>
+                                  </Card>
+                                </Col>
+                              </Row>
 
-                            <Divider />
-                          </Panel>
-                        ))}
+                              <Row type="flex" justify='start' style={{ marginTop: 10 }}>
+                                <Col span={24}>
+                                  <Text strong>Opções</Text>
+                                </Col>
+                                <Col span={24}>
+                                  <div style={{ display: 'flex', flexDirection: 'column', marginLeft: 20 }}>
+                                    {question.options.map((option, index) => {
+                                      const code = 'a'.charCodeAt(0);
+                                      const letterOption = String.fromCharCode(code + index);
+                                      const typeCheck = option.isCorrect === true ? 'success' : 'danger';
+                                      return (
+                                        <div className={option.userCheck === true ? `question-option-check-${typeCheck}` : null}>
+                                          <Text
+                                            type={typeCheck}
+                                          >
+                                            {letterOption.toUpperCase()}. {option.description} ({option.totalOptionAnswersPercentage}%)
+                                          </Text>
+                                        </div>
+                                      )
+                                    })}
+                                  </div>
+                                </Col>
+                              </Row>
+
+                              <Divider />
+                            </Panel>
+                          )
+                        })}
                       </Collapse>
                     </Col>
                   </Row>
